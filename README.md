f0994f669a8b3ec211f5533c573318d99b4a9094
Samuel Marks <samuelmarks@gmail.com>
2014-07-31 15:44:57 +1000

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+rest-api
+========
+
+Bottle REST API which merges all other apps into it. This is the one you e.g.: put behind uwsgi.
---
+++ namespace_rest_api.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from bottle import Bottle, response
+from oauth2_bottle_app import oauth2_app, __version__ as oauth2_app_version
+from namespace_user_api import user_api, __version__ as user_api_version
+
+rest_api = Bottle(catchall=False, autojson=True)
+rest_api.merge(oauth2_app)
+rest_api.merge(user_api)
+
+__version__ = '0.0.1'
+
+
+@rest_api.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'  # Take out '*' in production!
+
+
+@rest_api.route('/api')
+@rest_api.route('/api/status')
+def status():
+    return {'oauth2_app_version': oauth2_app_version,
+            'user_api_version': user_api_version,
+            'rest_api_version': __version__,
+            'server_time': datetime.now().strftime("%I:%M%p on %B %d, %Y")}
+
+
+if __name__ == '__main__':
+    rest_api.run(host='0.0.0.0', port=5555, debug=True)
