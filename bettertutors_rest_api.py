745716097ba3b667a4fdc5087f0ac3aeab2057ab
Samuel Marks <samuelmarks@gmail.com>
2015-02-16 22:40:44 +1100

    Had to include sql_models in requirements. It should've brought it in with pip, but didn't...

--- bettertutors_rest_api.py
+++ bettertutors_rest_api.py
@@ -4,11 +4,12 @@ from sys import platform
 
 from bottle import Bottle, response
 from bettertutors_user_api import user_app, __version__ as user_api_version
+from bettertutors_sql_models import __version__ as sql_models_version
 
 rest_api = Bottle(catchall=False, autojson=True)
 rest_api.merge(user_app)
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 @rest_api.hook('after_request')
@@ -21,6 +22,7 @@ def enable_cors():
 def status():
     return {'rest_api_version': __version__,
             'user_api_version': user_api_version,
+            'sql_models_version': sql_models_version,
             'server_time': datetime.now().strftime("%I:%M%p on %B %d, %Y")}
 
 
