da1829c311633aeceef4f1f3ca3b59002bd5cf6d
Samuel Marks <samuelmarks@gmail.com>
2015-02-18 00:29:32 +1100

    Reintroducing requirements.txt

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name='bettertutors_rest_api',
-        version='0.2.4',
+        version='0.2.5',
         author='Samuel Marks',
         py_modules=['bettertutors_rest_api'],
         test_suite='tests',
         install_requires=[
-            'bottle', 'webtest', 'gunicorn', 'bettertutors_user_api'
-        ],
-        dependency_links=[
-            'https://github.com/bettertutors/user-api/archive/master.zip#egg=bettertutors_user_api'
+            'bottle', 'webtest', 'gunicorn'
         ]
     )
