48b7f7d8e8c438cd88fc08a46d48b006394ea52a
Samuel Marks <samuelmarks@gmail.com>
2015-02-20 00:53:08 +1100

    Putting dependencies back in requirements.txt

--- setup.py
+++ setup.py
@@ -3,11 +3,8 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name='bettertutors_rest_api',
-        version='0.2.11',
+        version='0.2.12',
         author='Samuel Marks',
         py_modules=['bettertutors_rest_api'],
-        test_suite='tests',
-        install_requires=[
-            'bottle', 'webtest', 'gunicorn'
-        ]
+        test_suite='tests'
     )
