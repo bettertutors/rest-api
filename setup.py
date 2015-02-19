9381feaa19d202f7bfd40c7b03d7a0f49a8b984a
Samuel Marks <samuelmarks@gmail.com>
2015-02-20 00:23:28 +1100

    More experimentation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name='bettertutors_rest_api',
-        version='0.2.9',
+        version='0.2.10',
         author='Samuel Marks',
         py_modules=['bettertutors_rest_api'],
         test_suite='tests',
