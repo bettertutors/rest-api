4c74ade488e824f8b2cf11a2768ca13f171655f5
Samuel Marks <samuelmarks@gmail.com>
2015-02-20 23:36:50 +1100

    Upgrade all deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     package_name = 'bettertutors_rest_api'
     setup(
         name=package_name,
-        version='0.2.14',
+        version='0.2.15',
         author='Samuel Marks',
         py_modules=[package_name],
         test_suite='tests'
