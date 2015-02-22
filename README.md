19b3a617218d07ef13a441c3224ff7ec0e21318e
Samuel Marks <samuelmarks@gmail.com>
2015-02-22 21:27:50 +1100

    Tested with gunicorn binary, and updated version function

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ rest-api
 ========
 
 Bottle REST API which merges all other apps into it. This is the one you e.g.: put behind uwsgi.
+
+For deployment options, Google it or see examples with gunicorn and upstart in my sql-models repository.
--- bettertutors_rest_api.py
+++ bettertutors_rest_api.py
@@ -26,10 +26,12 @@ def get_version_of(package):
             raise DistributionNotFound
 
     except DistributionNotFound:
-        if package + '.py' == __file__:
-            return (lambda s: s[s.find("'") + 1:s.rfind("'")])(filter(lambda l: l.startswith('version'),
-                                                                      map(lambda l: l.strip(),
-                                                                          open('setup.py').readlines()))[0])
+        if path.basename(__file__).startswith(package):
+            return Distribution(version=(lambda s: s[s.find("'") + 1:s.rfind("'")])(
+                filter(lambda l: l.startswith('version'),
+                       map(lambda l: l.strip(),
+                           open(path.join(path.dirname(__file__), 'setup.py')).readlines()))[0]
+            ))
         return Distribution(version='"{package_name}" not found installed...'.format(package_name=package))
 
     return _dist
